fa2dd192e3a545f4fc14af91dfd010a065f123b3
sajjas <sajjasgit@gmail.com>
2026-05-05 18:30:34 -0400

    feat(#69): add shareable /report/:id route and ReportPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReportPage from './components/ReportPage';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/report/:id" element={<ReportPage />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
